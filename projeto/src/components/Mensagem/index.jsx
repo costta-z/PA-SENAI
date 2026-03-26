3ff018fc0fb8b6cfa75ff49dd174a51a8562d29b
Daniel costa <danielzaide2008@gmail.com>
2026-03-26 16:55:00 -0300

    app.jsx

---
+++ projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+function Mensagem() {
+    return (
+        <div className='mensagem'>
+            <h1>Componente</h1>
+            <p>Estes é um componente com uma mensagem simples.</p>
+        </div>
+    )    
+}
+
+export default Mensagem
